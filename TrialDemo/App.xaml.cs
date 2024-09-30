7f3c6e06a8369dcebe96c9dc39c6848176311c75
202117 <202117@K1210-002>
2024-09-30 19:14:15 +0300

    Добавьте файлы проекта.

---
+++ TrialDemo/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TrialDemo.Database;
+
+namespace TrialDemo
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Trial_Demo_420_KamilEntities DB = new Trial_Demo_420_KamilEntities();
+    }
+}
---
+++ TrialDemo/Pages/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrialDemo.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка.");
+            }
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Application.Current.Shutdown();
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка.");
+            }
+        }
+
+        private void ServicesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NavigationService.Navigate(new ServicesPage());
+            }
+            catch
+            {
+                MessageBox.Show("Произошла ошибка.");
+            }
+        }
+    }
+}
